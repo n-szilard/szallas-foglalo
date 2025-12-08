d745ea527f1ce257503f587360089f9ae0376a55
n-szilard <nagyapatiszilard@turr.hu>
2025-12-08 12:20:59 +0100

    SF-5 Értesítések

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/system/navbar/navbar.component';
 import { FooterComponent } from './components/system/footer/footer.component';
+import { MessageComponent } from './components/system/message/message.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, MessageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/system/message/message.component.ts
+++ src/app/components/system/message/message.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Message } from '../../../interfaces/message';
+import { MessageService } from '../../../services/message.service';
 
 @Component({
   selector: 'app-message',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss'
 })
-export class MessageComponent {
+export class MessageComponent implements OnInit{
 
+  message: Message | null = null;
+
+  constructor(private messageService: MessageService) {}
+
+
+  ngOnInit(): void {
+    this.messageService.message$.subscribe(msg => {
+      this.message = msg;
+      switch (this.message?.severity) {
+        case 'info': {
+          this.message.icon = 'bi-info-circle-fill';
+          break;
+        } case 'warning': {
+          this.message.icon = 'bi-exclamation-triangle-fill';
+          break;
+        } case 'success': {
+          this.message.icon = 'bi-check';
+          break;
+        } case 'danger': {
+          this.message.icon = 'bi-x-circle-fill';
+          break;
+        } default: {
+          break;
+        }
+      }
+    });
+  }
 }
