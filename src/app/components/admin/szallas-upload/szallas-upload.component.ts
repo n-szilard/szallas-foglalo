24fb09b23b224abf4a7a7c0d020ed87a3d268328
n-szilard <nagyapatiszilard@turr.hu>
2025-12-09 11:49:59 +0100

    SF-10 Szállás képek feltöltés

--- src/app/components/admin/szallas-upload/szallas-upload.component.ts
+++ src/app/components/admin/szallas-upload/szallas-upload.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { Accomodation } from '../../../interfaces/accomodation';
+import { FormsModule } from '@angular/forms';
+import { ApiService } from '../../../services/api.service';
+import { MessageService } from '../../../services/message.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-szallas-upload',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './szallas-upload.component.html',
   styleUrl: './szallas-upload.component.scss'
 })
 export class SzallasUploadComponent {
 
+  selectedFiles?: FileList;
+
+  newHotel: Accomodation = {
+    id: 0,
+    name: '',
+    description: '',
+    address: '',
+    capacity: 0,
+    basePrice: 0,
+    active: false
+  }
+
+  constructor(
+    private api: ApiService,
+    private message: MessageService,
+    private auth: AuthService
+  ) {}
+
+  upload() {
+
+  }
+
+  clearPictures() {
+    
+  }
+
+  uploadFile() {
+
+  }
+
+
+  onFileSelected(event: any) {
+    this.selectedFiles = event.target.files;
+  }
 }
