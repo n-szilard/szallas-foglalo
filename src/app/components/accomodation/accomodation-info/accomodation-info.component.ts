e2462b8415696da9430cf315404b0506f19e2fdc
Andor1298 <palinkasandor@turr.hu>
2026-01-06 09:01:46 +0100

    SF-11 Szállás további információja oldal

--- src/app/components/accomodation/accomodation-info/accomodation-info.component.ts
+++ src/app/components/accomodation/accomodation-info/accomodation-info.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { ApiService } from '../../../services/api.service';
+import { Accomodation } from '../../../interfaces/accomodation';
 
 @Component({
   selector: 'app-accomodation-info',
@@ -11,7 +13,31 @@ import { RouterModule } from '@angular/router';
   templateUrl: './accomodation-info.component.html',
   styleUrl: './accomodation-info.component.scss'
 })
-export class AccomodationInfoComponent {
+export class AccomodationInfoComponent implements OnInit {
+  accomodations: Accomodation[] = [];
+    selectedAccomodation: Accomodation = {
+      id: 0,
+      name: '',
+      description: '',
+      address: '',
+      capacity: 0,
+      basePrice: 0,
+      active: false
+    }
+  id!: number;
+  constructor(
+    private api: ApiService,
+    private route: ActivatedRoute){}
+  ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get('id'))
+    this.selectAcc(this.id);
+  }
+
+  selectAcc(id: number) {
+    this.api.selectOne('accomodations', id).then(res => {
+      this.selectedAccomodation = res.data[0];
+    })
+  }
   hotel = {
     name: "Miami Beach",
     rating: 8.2,
