98f7694b6c2324b211d061a49c49e31650bd4aed
n-szilard <nagyapatiszilard@turr.hu>
2026-01-06 09:01:25 +0100

    Képek feltöltése

--- src/app/components/admin/szallas-upload/szallas-upload.component.ts
+++ src/app/components/admin/szallas-upload/szallas-upload.component.ts
@@ -30,43 +30,55 @@ export class SzallasUploadComponent {
     private api: ApiService,
     private message: MessageService,
     private auth: AuthService
-  ) {}
+  ) { }
 
   upload() {
+
     if (this.newHotel.name == '' || this.newHotel.description == '' || this.newHotel.address == '' || this.newHotel.capacity == 0 || this.newHotel.basePrice == 0) {
       this.message.show('danger', 'Hiba', 'Nem adtál meg minden adatot!')
       return;
     }
 
-    if (this.newHotel.capacity <= 0 ||  this.newHotel.basePrice <= 0) {
+    if (this.newHotel.capacity <= 0 || this.newHotel.basePrice <= 0) {
       this.message.show('danger', 'Hiba', 'Nem megfelelő adatokat adtál meg!');
       return;
     }
 
     this.api.insert('accomodations', this.newHotel).then(res => {
+      this.newHotel.id = res.data.insertId
       this.message.show('success', 'Ok', 'A szállás hozzáadva');
     })
 
     if (this.selectedFiles.length > 0) {
-      this.uploadFiles();
+      this.uploadFiles(this.newHotel.id);
+    }
+
+    this.newHotel = {
+      id: 0,
+      name: '',
+      description: '',
+      address: '',
+      capacity: 0,
+      basePrice: 0,
+      active: false
     }
   }
 
   clearPictures() {
-    
+
   }
 
 
   //TODO: kép feltöltés
-  uploadFiles() {
+  uploadFiles(accomodationId: number) {
     const formData = new FormData();
 
     this.selectedFiles?.forEach(file => {
       formData.append('images', file);
     });
 
-    this.api.upload(formData).then(res => {
-      
+    this.api.upload(formData, accomodationId).then(res => {
+
     })
   }
 
