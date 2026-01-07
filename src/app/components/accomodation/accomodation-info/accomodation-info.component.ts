5e86e080bfaac6be9269b3d5ba01ca4d9cdd3e29
Andor1298 <palinkasandor@turr.hu>
2026-01-07 08:23:12 +0100

    fölös cucc

--- src/app/components/accomodation/accomodation-info/accomodation-info.component.ts
+++ src/app/components/accomodation/accomodation-info/accomodation-info.component.ts
@@ -38,40 +38,4 @@ export class AccomodationInfoComponent implements OnInit {
       this.selectedAccomodation = res.data[0];
     })
   }
-  hotel = {
-    name: "Miami Beach",
-    rating: 8.2,
-    reviewText: "Nagyon jó (7433 értékelés)",
-    address: "6500 Baja, Miami Beach",
-    price: 40000,
-
-    features: [
-      { title: "Nagyszerű elhelyezkedés", desc: "A vendégek kedvelik a környéket." },
-      { title: "Közeli látnivalók", desc: "Sétatávolságra a tengerpart." },
-      { title: "Wellness és kényelem", desc: "Spa, medencék, éttermek." }
-    ],
-
-    nearby: [
-      { place: "Miami tengerpart", time: "1 perc" },
-      { place: "Szálloda", time: "1 perc" },
-      { place: "BP repülőtér", time: "150 perc autóval" }
-    ],
-
-    amenities: [
-      "Strand hozzáférés",
-      "3 kültéri medence",
-      "Reggeli elérhető",
-      "Wellness szolgáltatások",
-      "Üzleti központ"
-    ],
-
-    description:
-      "Luxus vízparti hotel, vásárlási és szórakozási lehetőségekkel a közelben.",
-
-    bottomImages: [
-      '/assets/images/slider_01.jpg',
-      '/assets/images/slider_01.jpg',
-      '/assets/images/slider_01.jpg'
-    ]
-  };
 }
